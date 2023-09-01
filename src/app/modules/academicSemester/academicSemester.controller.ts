d2c43b85f65fb4d073143c647ccb8c19139247fa
codeCampWithSifat <sayedhossainsifat300@.com>
2023-09-02 02:02:28 +0600

    first commit

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,57 @@
+import { AcademicSemester } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterService } from './academicSemester.service';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester Created Successfully',
+    data: result,
+  });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  //   console.log(req.query);
+
+  const filters = pick(req.query, [
+    'searchTerm',
+    'code',
+    'startMonth',
+    'endMonth',
+  ]);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  //   console.log('filters', filters);
+  //   console.log('options', options);
+
+  const result = await AcademicSemesterService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester Created Successfully',
+    data: result.data,
+    meta: result.meta,
+  });
+});
+
+const getDataById = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await AcademicSemesterService.getDataById(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get A Academic Semester Data Successfully',
+    data: result,
+  });
+});
+
+export const AcademicSemesterController = {
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+};
---
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterZodValidation } from './academicSemester.validation';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  validateRequest(AcademicSemesterZodValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
+
+router.get('/', AcademicSemesterController.getAllFromDB);
+router.get('/:id', AcademicSemesterController.getDataById);
+
+export const AcademicSemesterRoutes = router;
